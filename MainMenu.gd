c8a15f40a4efbe10640f5b7e73796cc79dfe3dd7
Slikek <god.boy13243546@gmail.com>
2024-05-19 04:34:07 +0300

    +player +mainmenu

---
+++ MainMenu.gd
@@ -0,0 +1,4 @@
+extends Node2D
+
+func _on_button_button_down():
+	get_tree().change_scene_to_file("res://node_2d.tscn")
---
+++ player.gd
@@ -0,0 +1,29 @@
+extends Area2D
+
+@export var speed = 250 # How fast the player will move (pixels/sec).
+var screen_size # Size of the game window.
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+	print(screen_size)
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed('ui_right'):
+		velocity.x += 1
+	if Input.is_action_pressed('ui_left'):
+		velocity.x -= 1
+	if Input.is_action_pressed('ui_down'):
+		velocity.y += 1
+	if Input.is_action_pressed('ui_up'):
+		velocity.y -= 1
+	
+	if velocity.length() > 0:
+		if Input.is_action_pressed("sprint"):
+			velocity = velocity.normalized() * speed * 1.5
+		else:
+			velocity = velocity.normalized() * speed
+		
+	position += velocity * delta
