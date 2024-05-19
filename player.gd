4860a31a10f98583638c0ee17b92914722528d10
Slikek <god.boy13243546@gmail.com>
2024-05-19 05:41:05 +0300

    нет
    а ты

--- player.gd
+++ player.gd
@@ -7,23 +7,22 @@ var screen_size # Size of the game window.
 func _ready():
 	screen_size = get_viewport_rect().size
 	print(screen_size)
-	
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var velocity = Vector2.ZERO # The player's movement vector.
 	if Input.is_action_pressed('ui_right'):
-		velocity.x += 2
+		velocity.x += 1
 	if Input.is_action_pressed('ui_left'):
-		velocity.x -= 2
+		velocity.x -= 1
 	if Input.is_action_pressed('ui_down'):
-		velocity.y += 2
+		velocity.y += 1
 	if Input.is_action_pressed('ui_up'):
-		velocity.y -= 2
-	
+		velocity.y -= 1
 	if velocity.length() > 0:
 		if Input.is_action_pressed("sprint"):
 			velocity = velocity.normalized() * speed * 1.5
 		else:
 			velocity = velocity.normalized() * speed
-		
+
 	position += velocity * delta
