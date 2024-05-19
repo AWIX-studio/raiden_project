274607787246484488a500d2618f07d992f50c2b
FIXS <forixsins@gmail.com>
2024-05-19 05:40:31 +0300

    Я сосал члены
    А ты?

--- player.gd
+++ player.gd
@@ -12,13 +12,13 @@ func _ready():
 func _process(delta):
 	var velocity = Vector2.ZERO # The player's movement vector.
 	if Input.is_action_pressed('ui_right'):
-		velocity.x += 1
+		velocity.x += 2
 	if Input.is_action_pressed('ui_left'):
-		velocity.x -= 1
+		velocity.x -= 2
 	if Input.is_action_pressed('ui_down'):
-		velocity.y += 1
+		velocity.y += 2
 	if Input.is_action_pressed('ui_up'):
-		velocity.y -= 1
+		velocity.y -= 2
 	
 	if velocity.length() > 0:
 		if Input.is_action_pressed("sprint"):
