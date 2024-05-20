87fed6872bdbff10cb42c46dfd7785e7373cd5fc
Slikek <god.boy13243546@gmail.com>
2024-05-21 00:33:35 +0300

    done

--- player.gd
+++ player.gd
@@ -7,7 +7,7 @@ func _ready():
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	velocity = Vector2.ZERO # The player's movement vector.
 	if Input.is_action_pressed('ui_right'):
 		velocity.x += 1
@@ -23,4 +23,4 @@ func _process(delta):
 		else:
 			velocity = velocity.normalized() * speed
 
-	position += velocity * delta
+	move_and_slide()
