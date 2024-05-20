48a52db4bab5681984a8e6dbe5e2c019a5fad25d
FIXS <forixsins@gmail.com>
2024-05-20 23:07:54 +0300

    Add_Settings_Menu

--- MainMenu.gd
+++ MainMenu.gd
@@ -2,3 +2,7 @@ extends Node2D
 
 func _on_button_button_down():
 	get_tree().change_scene_to_file("res://node_2d.tscn")
+
+
+func _on_button_2_button_down():
+	get_tree().change_scene_to_file("res://Settings.tscn")
---
+++ Settings.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+
+
+func _on_button_button_down():
+	pass
+
+
+func _on_button_2_button_down():
+	get_tree().change_scene_to_file("res://MainMenu.tscn")
