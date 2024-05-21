244b9ae28caedfdb83d5038cfe375b1b9eb0980c
FIXS <forixsins@gmail.com>
2024-05-21 03:49:12 +0300

    Test_room_is_done
    гыыыыыыыыыы

--- MainMenu.gd
+++ MainMenu.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 func _on_button_button_down():
-	get_tree().change_scene_to_file("res://Test_Scene_1.tscn")
+	get_tree().change_scene_to_file("res://Test 1.tscn")
 
 
 func _on_button_2_button_down():
