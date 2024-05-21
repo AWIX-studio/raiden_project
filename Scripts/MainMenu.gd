2f265626abdb1d54e34f0994b20769ad8bdc6777
FIXS <forixsins@gmail.com>
2024-05-21 22:35:45 +0300

    Quit_Button

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -6,3 +6,7 @@ func _on_button_button_down():
 
 func _on_button_2_button_down():
 	get_tree().change_scene_to_file("res://Scenes/Menu/SettingsMenu.tscn")
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
